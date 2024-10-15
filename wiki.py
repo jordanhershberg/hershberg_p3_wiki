d8e8990119608e5ca69735789c85fb826759205a
Jordan Hershberg <maj1hershberg@gmail.com>
2024-10-15 20:39:24 +0000

    added stuff

--- wiki.py
+++ wiki.py
@@ -1,3 +1,4 @@
+from queue import Queue
 import wikipediaapi
 import time
 
@@ -17,7 +18,6 @@ def fetch_links(page):
 def wikipedia_game_solver(start_page, target_page):
     print("Working on it...")
     start_time = time.time()
-
     queue = Queue() # queue for which items to check next
     visited = set() # keeps track of visited links
     parent = {} # dictionary to keep track of parent
@@ -33,6 +33,28 @@ def wikipedia_game_solver(start_page, target_page):
         current_page_title = queue.get()
         if current_page_title == target_page.title:
             break
+        current_page = wiki.page(current_page_title)
+        links = fetch_links(current_page)
+        
+        for link in links:
+            if link not in visited:
+                queue.put(link)
+                visited.add(link)
+                parent[link] = current_page_title
+            
+
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title = parent[page_title]
+    
+    path.append(start_page.title)
+    path.reverse()
+
+    end_time = time.time()
+    print("this algorithm took", end_time - start_time, "seconds.")
+    return path
 
     
 
@@ -40,5 +62,7 @@ def wikipedia_game_solver(start_page, target_page):
 
 # start and end pages for our wikipedia game solver
 start_page = wiki.page("Rocket League")
-target_page = wiki.page("First-person shooter")
-wikipedia_game_solver(start_page, target_page)
+target_page = wiki.page("Racing")
+path = wikipedia_game_solver(start_page, target_page)
+print (path)
+
